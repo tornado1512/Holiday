99ba7581085610bc4ee7356d35eb1d0c94e78cf0
tornado1512 <vagrahari943@gmail.com>
2020-07-05 09:24:42 +0530

    chnges

--- js/package.js
+++ js/package.js
@@ -1,22 +1,26 @@
 window.onload=initAll;
 
 function initAll(){
+	alert("hello");	
 	getAllElements();
 	setAllActions();
 }
 
 var pac_box,pic;
 function getAllElements(){
+	
 	pacBox = document.getElementById("pac_box");
 	pic = document.getElementById("pic");
 }
 
 function setAllActions(){
 	pic.onclick=add;
+	
 }
 
 var i=2;
 function add(){
+	alert(00);
 	var br=document.createElement('br');
 	pacBox.appendChild(br);
 	var inp=document.createElement("input");
